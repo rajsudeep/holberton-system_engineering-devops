d0d9fb75437a1f707b0ae57c2b268165557a80c7
Sudeep Raj <36677071+rajsudeep@users.noreply.github.com>
2019-12-09 16:43:03 -0800

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,9 @@
+#create a file in /tmp
+
 file { '/tmp/holberton':
-    ensure  => 'file',
-    owner   => 'www-data',
-    group   => 'www-data',
-    mode    => '0744',
-    content => 'I love Puppet',
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
