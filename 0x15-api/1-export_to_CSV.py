425f16881fdb11add53bd2c8a45c11dfbda43534
rajsudeep <sudrocks@gmail.com>
2020-02-03 21:11:47 +0000

    Add task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Finds employee information """
+from requests import get
+from sys import argv
+import csv
+
+
+def get_employee_info_csv(url, user_id):
+    """ Print completed tasks of corresponding employee """
+    user = get(url + 'users/' + user_id).json()
+    user_name = user['name']
+    todos = get(url + 'todos?userId={}'.format(user_id)).json()
+
+    try:
+        with open('{}.csv'.format(user_id), mode='w') as f:
+            write = csv.writer(f, quoting=csv.QUOTE_ALL)
+            for task in todos:
+                write.writerow([
+                    user_id,
+                    user_name,
+                    task.get('completed'),
+                    task.get('title'),
+                ])
+    except IOError:
+        print("I/O error")
+
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/'
+    user_id = argv[1]
+    get_employee_info_csv(url, user_id)
