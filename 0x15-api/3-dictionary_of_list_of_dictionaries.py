6409f33f20aaf1abdd47a3e090eb901220dd93be
rajsudeep <sudrocks@gmail.com>
2020-02-04 02:37:59 +0000

    Use only tasks specific to user

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -14,11 +14,13 @@ def get_all(url):
     for user in users:
         tasks = []
         for task in todos:
-            task_info = {}
-            task_info['title'] = task['title']
-            task_info['username'] = user['username']
-            task_info['completed'] = task['completed']
-            tasks.append(task_info)
+            if user['id'] == task['userId']:
+                task_info = {
+                    'username': user['name'],
+                    'completed': task['completed'],
+                    'task': task['title'],
+                }
+                tasks.append(task_info)
         dict_tasks[user['id']] = tasks
     with open('todo_all_employees.json', 'w') as f:
         json.dump(dict_tasks, f)
