cff284be77104f453f25c14b90e69f12b0aa6ca9
rajsudeep <sudrocks@gmail.com>
2020-02-07 00:43:24 +0000

    Add task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,20 +4,17 @@ import requests
 
 
 def recurse(subreddit, hot_list=[], after=None):
-    """ Returns a list containing the titles of all hot articles """
+    """ Returns a list of hot titles """
     headers = {'User-Agent': ''}
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     if after:
-        url += '?=after={}'.format(after)
-    response = requests.get(url, headers=headers)
-    if response.status_code != 200:
-        return
-    posts_x = response.json()
-    posts = posts_x['data']['children']
-    for post in posts:
-        hot_list.append(post['data']['title'])
-    posts_after = posts_x['data']['after']
-    if posts_after:
-        return recurse(subreddit, hot_list=hot_list, after=posts_after)
-    else:
+        url += '?after={}'.format(after)
+    try:
+        response = requests.get(url, headers=headers).json().get('data')
+        for post in response.get('children'):
+            hot_list.append(post.get('data').get('title'))
+        after = response.get('after')
+        recurse(subreddit, hot_list, after)
         return hot_list
+    except:
+        return None
